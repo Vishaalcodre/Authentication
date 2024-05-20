c4841530f10e9bd32c5c6e23c8b3fd2591ae7a24
Vishaalcodre <vishaa369@gmail.com>
2024-05-21 00:17:09 +0530

    Level 3

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-import bcrypt, { hash } from "bcrypt";
+import bcrypt from "bcrypt";
+import session from "express-session";
+import passport from "passport";
+import { Strategy } from "passport-local";
 
 const app = express();
 const port = 3000;
-const saltRounds = 6;
+const saltRounds = 10;
+
+app.use(session({
+  secret: "CONFIDENTIAL",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 5, //5 minutes expiry
+  }
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const db = new pg.Client({
   user: "postgres",
@@ -15,9 +31,6 @@ const db = new pg.Client({
   port: 5432,
 });
 db.connect();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
@@ -29,6 +42,15 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
+});
+
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()){
+    res.render("secrets.ejs");
+  }
+  else {
+    res.redirect("/login");
+  }
 });
 
 app.post("/register", async (req, res) => {
@@ -43,61 +65,73 @@ app.post("/register", async (req, res) => {
     if (checkResult.rows.length > 0) {
       res.send("Email already exists. Try logging in.");
     } else {
+      //hashing the password and saving it in the database
       bcrypt.hash(password, saltRounds, async (err, hash) => {
         if (err) {
-          console.log("Error in hashing password", err);
-        }
-
-        else {
+          console.error("Error hashing password:", err);
+        } else {
+          console.log("Hashed Password:", hash);
           const result = await db.query(
-            "INSERT INTO users (email, password) VALUES ($1, $2)",
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
             [email, hash]
           );
-          console.log(result);
+          const user = result.rows[0];
+          req.login(user, (err) => {
+            console.log(err);
+            res.redirect("/secrets");
+          })
           res.render("secrets.ejs");
         }
-      })
-      
+      });
     }
   } catch (err) {
     console.log(err);
   }
 });
 
-app.post("/login", async (req, res) => {
-  const email = req.body.username;
-  const password = req.body.password;
-  
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/secrets",
+  failureRedirect: "/login",
+}));
+
+passport.use(new Strategy(async function (username, password, cb) {
+  console.log(username);
+
   try {
     const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
+      username,
     ]);
     if (result.rows.length > 0) {
       const user = result.rows[0];
-      const storedPassword = user.password;
-
-      bcrypt.compare(password, storedPassword, (err, result) => {
+      const storedHashedPassword = user.password;
+      bcrypt.compare(password, storedHashedPassword, (err, result) => {
         if (err) {
-          console.log("Error comparing password", err);
-        }
-        else {
-          console.log("The status of comparing password is: ", result);
-          
+          return cb(err)
+        } else {
           if (result) {
+            return cb(null, user);
             res.render("secrets.ejs");
-          }
-          else {
-            res.send("Incorrect Password");
+          } else {
+            return cb(null, false);
           }
         }
-      })
+      });
     } else {
-      res.send("User not found");
+      return cb("User not found");
     }
   } catch (err) {
-    console.log(err);
+    return cb(err);
   }
-});
+
+  passport.serializeUser((user, cb) => {
+    cb(null, user);
+  });
+
+  passport.deserializeUser((user, cb) => {
+    cb(null, user);
+  });
+
+}))
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
