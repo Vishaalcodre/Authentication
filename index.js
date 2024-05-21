6de96d387d8b11235a3237def50135966a9181b1
Vishaalcodre <vishaa369@gmail.com>
2024-05-21 15:32:18 +0530

    Level 5

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import pg from "pg";
 import bcrypt from "bcrypt";
 import passport from "passport";
 import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
 import session from "express-session";
 import env from "dotenv";
 
@@ -19,6 +20,7 @@ app.use(
     saveUninitialized: true,
   })
 );
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -47,22 +49,29 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/logout", (req, res) => {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
+  req.logout((err) => {
+    if (err) console.log(err);
     res.redirect("/");
   });
 });
 
 app.get("/secrets", (req, res) => {
-  // console.log(req.user);
+  console.log(req.user);
   if (req.isAuthenticated()) {
     res.render("secrets.ejs");
   } else {
     res.redirect("/login");
   }
 });
+
+app.get("/auth/google", passport.authenticate("google", {
+  scope: ["profile", "email"],
+}));
+
+app.get("/auth/google/secrets", passport.authenticate("google", {
+  successRedirect: "/secrets",
+  failureRedirect: "/login",
+}));
 
 app.post(
   "/login",
@@ -137,6 +146,33 @@ passport.use(
     }
   })
 );
+
+passport.use("google", new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+},
+async (accessToken, refreshToken, profile, cb) => {
+  console.log(profile);
+  try {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [profile.email]);
+
+    if (result.rows.length == 0) {
+      const newUser = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)",
+      [profile.email, "google"]);
+
+      cb(null, newUser.rows[0]);
+    }
+    else {
+      cb(null, result.rows[0]);
+    }
+    
+  } catch (err) {
+    cb(err);
+  }
+}
+));
 
 passport.serializeUser((user, cb) => {
   cb(null, user);
